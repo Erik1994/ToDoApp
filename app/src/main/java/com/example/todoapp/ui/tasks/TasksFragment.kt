b6e2aa357df334f2458ee271b813dbae58007a2a
Erik Antonyan <erik.antonyan1994@gmail.com>
2021-01-31 01:43:51 +0400

    Implement TaskApdapter and search functionality

--- app/src/main/java/com/example/todoapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/todoapp/ui/tasks/TasksFragment.kt
@@ -1,13 +1,83 @@
 package com.example.todoapp.ui.tasks
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentTasksBinding
+import com.example.todoapp.util.onQueryTextChanging
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TasksFragment: Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TasksViewModel by viewModels()
+    private val taskAdapter = TaskAdapter()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+        val binding = FragmentTasksBinding.bind(view)
+        initRecyclerView(binding)
+        observeData()
+    }
+
+    private fun initRecyclerView(binding: FragmentTasksBinding) {
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+    }
+
+    private fun observeData() {
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_taks, menu)
+
+        val serachItem = menu.findItem(R.id.action_search)
+        val searchView = serachItem.actionView as SearchView
+
+        searchView.onQueryTextChanging {
+            viewModel.searchQuery.value = it
+        }
+
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_sort_by_name -> {
+                true
+            }
+
+            R.id.action_sort_by_date_created -> {
+                true
+            }
+
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+                true
+            }
+
+            R.id.action_delete_all_completed_tasks -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+
+        }
+    }
 
 }
--- app/src/main/java/com/example/todoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/tasks/TasksViewModel.kt
@@ -2,10 +2,22 @@ package com.example.todoapp.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.example.todoapp.data.TaskDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ): ViewModel() {
+
+    val searchQuery = MutableStateFlow("")
+
+    private val tasksFlow = searchQuery.flatMapLatest { //flatMapLastest is ekvivalent to switchMap for liveData
+        taskDao.getTasks(it)
+    }
+
+    val tasks = tasksFlow.asLiveData()
+
 
 }
