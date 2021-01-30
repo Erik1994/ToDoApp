2b74c5f30479e67bd27f91c70b119e3034cb351e
Erik Antonyan <erik.antonyan1994@gmail.com>
2021-01-30 22:21:41 +0400

    Add room db with dagger hilt di

---
+++ app/src/main/java/com/example/todoapp/di/AppModule.kt
@@ -0,0 +1,45 @@
+package com.example.todoapp.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.todoapp.data.TaskDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context,
+        callback: TaskDatabase.CallBack
+    ) = Room.databaseBuilder(
+        context,
+        TaskDatabase::class.java,
+        "task_database"
+    ).fallbackToDestructiveMigration()
+        .addCallback(callback)
+        .build()
+
+    @Provides
+    fun provideTaskDao(db: TaskDatabase) = db.taskDao()
+
+    @ApplicationScope
+    @Provides
+    @Singleton
+    fun provideApplicationScope() = CoroutineScope(SupervisorJob())
+
+}
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class ApplicationScope
